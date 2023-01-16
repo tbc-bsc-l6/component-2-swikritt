a2f0efb5a3165505a0131f7b94897b4fca238d09
swikritt <116990261+swikritt@users.noreply.github.com>
2023-01-16 15:42:17 +0545

    Create , Show and Edit buttons added

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.master')
 @section('content')
 <h1> List of products </h1>
+
+<a class="btn btn-success"href=" {{ route ('products.create')}}">Create</a>
+
 @empty ($products))
 <div class="alert  alert-warning">
     The list of products is empty
@@ -25,7 +28,11 @@
     <td>{{ $product->price }}</td>
     <td>{{ $product->stock }}</td>
     <td>{{ $product-> status }}</td>
-
+     <td> <a class="btn btn-link"href=" {{ route ('products.show', ['product' => $product->
+         id] ) }}">Show</a>
+         <a class="btn btn-link"href=" {{ route ('products.edit', ['product' => $product->
+         id] ) }}">Edit</a>
+        </td>
 
     
 </tr>
