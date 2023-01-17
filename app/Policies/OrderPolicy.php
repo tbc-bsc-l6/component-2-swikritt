f360389857547be692bb7a3a07bbaab401e91b17
swikritt <116990261+swikritt@users.noreply.github.com>
2023-01-17 17:54:42 +0545

    Order components

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Order;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Order $order)
+    {
+        //
+    }
+}
