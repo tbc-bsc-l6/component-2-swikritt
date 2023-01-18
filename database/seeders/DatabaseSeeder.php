d65b1913fb21be3fd24c94dee86f9b636f307e57
swikritt <116990261+swikritt@users.noreply.github.com>
2023-01-18 14:03:53 +0545

    Inserting simple model relationships within the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use App\Product;
+use App\Cart;
+use App\Image;
+use App\Order;
+use App\Payment;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +20,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $products = Product::factory(50)->create();
+
+        $users = User::factory(20)->create();
+
+        $orders = Order::factory(10)
+            ->make()
+            ->each (function ($order) use($users) {
+                $order->customer_id = $users->random()->id;
+                $order->save();
+
+                $payment = Payment::factory()->make();
+
+                // $payment->order_id = $order->id;
+                // $payment->save();
+                    //lravel handles foreign key automatically 
+                 $order->payment()->save($payment);
+            });
     }
 }
