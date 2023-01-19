d6799a7caffac3b679c4b58d7f94994565ed84a6
swikritt <116990261+swikritt@users.noreply.github.com>
2023-01-19 21:39:02 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MainController@index')->name('main');
 
-Route::get('profile','ProfileController@edit')->name('profile.edit');
-
-Route::get('profile','ProfileController@update')->name('profile.update');
 
 
 
